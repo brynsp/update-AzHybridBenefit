1b8c185fc22f6732f6b779a638aef4a9d3e156de
Bryn Spears <brynspears@microsoft.com>
2025-06-26 16:11:10 -0400

    Enhance Update-AzHybridBenefit script to discover Windows VMs and update license types for both VMs and SQL Server installations

--- src/powershell/Update-AzHybridBenefit.ps1
+++ src/powershell/Update-AzHybridBenefit.ps1
@@ -28,21 +28,20 @@ Write-Host "Found $($subscriptions.Count) subscriptions" -ForegroundColor Green
 
 # Get all Windows VMs from all subscriptions
 Write-Host 'Discovering Windows VMs…' -ForegroundColor Yellow
-$allVMs = [System.Collections.Generic.List[object]]::new()
-
 $jobs = foreach ($sub in $subscriptions) {
     Start-Job -ScriptBlock {
         param($subId)
         Import-Module Az.Accounts, Az.Compute
         Set-AzContext -SubscriptionId $subId | Out-Null
-        Get-AzVM | Where-Object { $_.StorageProfile.OSDisk.OSType -eq 'Windows' }
+        Get-AzVM -Status | Where-Object { $_.StorageProfile.OSDisk.OSType -eq 'Windows' }
     } -ArgumentList $sub.Id
 }
 
 $results = $jobs | Wait-Job | Receive-Job
 $jobs | Remove-Job
 
-# Add all VMs to the collection
+# Add all VMs to a collection
+$allVMs = [System.Collections.Generic.List[object]]::new()
 foreach ($vm in $results) {
     if ($vm) {
         $allVMs.Add($vm)
@@ -50,3 +49,39 @@ foreach ($vm in $results) {
 }
 
 Write-Host "Total Windows VMs: $($allVMs.Count)" -ForegroundColor Cyan
+
+# Process each VM in parallel - check and update license type
+$jobs = foreach ($vm in $allVMs) {
+    Start-Job -ScriptBlock {
+        param($vm)
+        Import-Module Az.Accounts, Az.Compute, Az.SqlVirtualMachine
+
+        # Set the context to the VM's subscription
+        $subscriptionId = $vm.Id.Split('/')[2]
+        Set-AzContext -SubscriptionId $subscriptionId | Out-Null
+
+        # Check to see if the OS license type is not 'Windows_Server'
+        $licenseType = $vm.LicenseType
+        if ($licenseType -ne 'Windows_Server') {
+            # Get the full VM object for update
+            $fullVm = Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+            $fullVm.LicenseType = 'Windows_Server'
+            $null = Update-AzVM -ResourceGroupName $vm.ResourceGroupName -VM $fullVm
+        }
+        
+        # Check to see if the VM also has SQL Server installed
+        $sqlVm = Get-AzSqlVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -ErrorAction SilentlyContinue
+        if ($sqlVm) {
+            $sqlLicense = $sqlVm.SqlServerLicenseType
+                
+            # Only update if not already AHUB or DR
+            if ($sqlLicense -ne 'DR' -and $sqlLicense -ne 'AHUB') {
+                $sqlVm.SqlServerLicenseType = 'AHUB'
+                $null = Update-AzSqlVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -LicenseType 'AHUB'
+            }
+        }
+    } -ArgumentList $vm
+}
+
+$results = $jobs | Wait-Job | Receive-Job
+$jobs | Remove-Job
